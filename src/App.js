a0847852a6c7247959a8d8673f3235d48cf907ef
Pyami <1pyami@gmail.com>
2021-06-13 14:29:35 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [showNav, setShowNav] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button className="nav" onClick={() => setShowNav(true)}>
+        |||
+      </button>
+      {showNav && (
+        <div className="sidbar">
+          <header className="header">
+            <h1 className="title">Codding Addict</h1>
+            <button onClick={() => setShowNav(false)}>X</button>
+          </header>
+          <div className="links">
+            <ul className="linkCont">
+              <li className="link">
+                <p>
+                  <span className="fa fa-home"></span>
+                  <span className="text">Home</span>
+                </p>
+              </li>
+              <li className="link">
+                <p>
+                  <span className="fa fa-users"></span>
+                  <span className="text">Team</span>
+                </p>
+              </li>
+              <li className="link">
+                <p>
+                  <span className="fa fa-folder-open"></span>
+                  <span className="text">Projects</span>
+                </p>
+              </li>
+              <li className="link">
+                <p>
+                  <span className="fa fa-calendar"></span>
+                  <span className="text">Calender</span>
+                </p>
+              </li>
+              <li className="link">
+                <p>
+                  <span className="fa fa-file"></span>
+                  <span className="text">Documents</span>
+                </p>
+              </li>
+            </ul>
+          </div>
+          <div className="social-links">
+            <ul className="social-link">
+              <li className="social">
+                <span className="fa fa-facebook"></span>
+              </li>
+              <li className="social">
+                <span className="fa fa-twitter"></span>
+              </li>
+              <li className="social">
+                <span className="fa fa-linkedin"></span>
+              </li>
+              <li className="social">
+                <span className="fa fa-diamond"></span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
+
+      <button className="openModal" onClick={() => setShowModal(true)}>
+        Show Modal
+      </button>
+
+      {showModal && (
+        <div className="modal">
+          <header className="header">
+            <h1 className="title">Modal</h1>
+            <button onClick={() => setShowModal(false)}>X</button>
+          </header>
+        </div>
+      )}
+    </>
   );
 }
 
